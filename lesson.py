c64df6cd3bcb059d0253af99c80bf51e2ce1f846
Hyun Joo Nam <hyunjoooo.nam@gmail.com>
2019-03-13 20:29:36 -0400

    wk1thur python basic

---
+++ lesson.py
@@ -0,0 +1,72 @@
+# python lesson.py
+
+# print 'Hello, world!'
+
+# print 'Hello'
+# print 'World'
+
+# print 'One', 'Two', 'Three'
+
+# print 'one',
+# print 'two',
+# print 'three',
+
+# import sys
+# sys.stdout.write('Hello')
+# sys.stdout.write('Hello')
+# sys.stdout.write('Hello')
+
+# print 'abc' + 'def'
+
+# print 'I am one line. \nI am another line'
+
+# verb = 'riding'
+# adjecitve = 'great'
+# sentence = 'Most doctors agree that bicycle %s is a/an %s form of exercise.' % (verb, adjecitve)
+# print sentence
+
+# name = 'Raj'
+# age = 5
+# sentence = '%s is %d years old.' % (name, age)
+# print sentence
+
+# name = 'Raj'
+# sentence = 'I am %s' % name
+# print sentence
+
+# string = '45'
+# num = int(string)
+# print num
+
+# name = raw_input("What's your name?")
+
+# age = int(raw_input("What's your age?"))
+# if age >= 21:
+#     print 'You are an adult.'
+# elif age >= 13:
+#     print 'You are a teenager.'
+# else:
+#     print 'You are a child.'
+
+# count = 0
+# while count < 10:
+#     count = count + 1
+#     print "count is now %d" % count
+# print "Done."
+
+# say = raw_input('Say when: ')
+# while say != 'when':
+#     print "Cheese"
+#     say = raw_input('Say when: ')
+
+
+# while True:
+#     say = raw_input('Say when: ')
+#     if say == 'when':
+#         break
+#     print "cheese"
+
+# if True:
+#     print "I <3 Python!"
+# if False:
+#     print "I hate Python!"
